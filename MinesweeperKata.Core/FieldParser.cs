43caffb540097359b8395e53e9ead19d5401020b
Paul Marfleet <paul.marfleet@gmail.com>
2016-04-23 20:32:04 +0100

    Created solution structure Implemented parsing of number of lines and columns

---
+++ MinesweeperKata.Core/FieldParser.cs
@@ -0,0 +1,22 @@
+﻿namespace MinesweeperKata.Core
+{
+    public class FieldParserResult
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+    }
+
+    public static class FieldParser
+    {
+        public static FieldParserResult Parse(string input)
+        {
+            var elements = input.Split(' ');
+
+            return new FieldParserResult
+            {
+                Lines = int.Parse(elements[0]),
+                Columns = int.Parse(elements[1])
+            };
+        }
+    }
+}
---
+++ MinesweeperKata.Tests/FieldParsingTests.cs
@@ -0,0 +1,30 @@
+﻿using MinesweeperKata.Core;
+using Xunit;
+
+namespace MinesweeperKata.Tests
+{
+    public class FieldParsingTests
+    {
+        [Theory]
+        [InlineData("4 4", 4)]
+        [InlineData("3 4", 3)]
+        [InlineData("2 4", 2)]
+        public void ShouldParseNumberOfLines(string input, int expectedLines)
+        {
+            var result = FieldParser.Parse(input);
+
+            Assert.Equal(expectedLines, result.Lines);
+        }
+
+        [Theory]
+        [InlineData("4 4", 4)]
+        [InlineData("4 3", 3)]
+        [InlineData("4 2", 2)]
+        public void ShouldParseNumberOfColumns(string input, int expectedColumns)
+        {
+            var result = FieldParser.Parse(input);
+
+            Assert.Equal(expectedColumns, result.Columns);
+        }
+    }
+}
